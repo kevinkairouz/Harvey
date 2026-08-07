7586693e97ad791fbd9196c6333fa883b91fa5fa
kevinkairouz <KM.Kairouz@gmail.com>
2026-08-07 17:02:27 -0400

    create state for agents short term memory

--- schema.py
+++ schema.py
@@ -1,6 +1,14 @@
 from dataclasses import dataclass 
-from typing import TypedDict, Annotated 
+from typing import TypedDict, Annotated, List
+from langchain.messages import HumanMessage, AIMessage 
 import operator 
 
 class HarveyState(TypedDict): 
-    ...
+    userQuery: list[HumanMessage]
+    harvey_response: list[AIMessage] 
+
+
+
+
+
+
